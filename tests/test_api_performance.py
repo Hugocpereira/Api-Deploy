57a94caba709d530bf2a6ae996a345256956be03
Hugocpereira <hugocp100@hotmail.com>
2024-11-04 12:37:19 -0300

    error fixed

--- tests/test_api_performance.py
+++ tests/test_api_performance.py
@@ -13,26 +13,31 @@ def client():
 def test_login_performance(benchmark, client):
     response = benchmark(client.post, '/login', json={
         'username': 'hugo',
-        'password': 'senha123'
+        'password': 'senhasenha'
     })
     assert response.status_code == 200
+    assert 'token' in response.json 
 
 def test_home_performance(benchmark, client):
     response = benchmark(client.get, '/')
     assert response.status_code == 200
 
 def test_consulta_performance(benchmark, client):
-    with app.app_context():  # Abre um contexto de aplicação
-        # Cria o token de acesso dentro do contexto da aplicação
+    with app.app_context():  
         access_token = create_access_token(identity="hugo")
         headers = {
             "Authorization": f"Bearer {access_token}"
         }
 
-    # Usa o benchmark para medir o desempenho da requisição de consulta
-    response = benchmark(client.get, '/consulta', headers=headers, query_string={
+    response = benchmark(client.get, '/api/consulta', headers=headers, query_string={
         'emp': '12345',
-        'emp': '2385'
-        
+        'ben': '2385'
     })
     assert response.status_code == 200
+
+def test_login_invalid_user(client):
+    response = client.post('/login', json={
+        'username': 'invalido',
+        'password': 'senhaerrada'
+    })
+    assert response.status_code == 401  
